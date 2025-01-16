5234c1f21d0c21de74e15aa04538a8659349f362
Ibo Ndure <orfanos@hotmail.de>
2025-01-16 21:02:45 +0100

    add Banner

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,41 @@
 "use client";
-
 import styled from "styled-components";
+import banner from "@/public/tüvimage.jpg";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header>
+    <>
+      <HeaderWrapper>
+        <Banner>
+          <Image
+            src={banner}
+            alt="TÜV Banner"
+            layout="fill"
+            objectFit="cover"
+          />
+        </Banner>
+      </HeaderWrapper>
       <WebsiteName>Kfz Zulassungsstelle</WebsiteName>
-    </header>
+    </>
   );
 }
 
+const HeaderWrapper = styled.header`
+  position: relative;
+  width: 100%;
+`;
+
+const Banner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 250px;
+`;
+
 const WebsiteName = styled.h1`
-  display: flex;
-  justify-content: center;
-  margin-top: 2rem;
-  padding: 2rem;
+  text-align: center;
+  margin-top: 0rem;
+  padding: 1rem;
+  font-size: 2rem;
+  color: white;
 `;
